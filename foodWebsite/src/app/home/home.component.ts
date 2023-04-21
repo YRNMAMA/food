e65cfb4e8df3f87b3b77aef2593329854deda724
Gningue Mamadou <91191658+YRNMAMA@users.noreply.github.com>
2023-04-21 13:05:05 +0000

    CORREZZIONE CSS

--- foodWebsite/src/app/home/home.component.ts
+++ foodWebsite/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  foods: string[]=[];
+
+  constructor(private fs:FoodService){}
+  
+
+
+ngOnInit(): void {
+
+  this.foods = this.fs.getAll(); // da 
+
+}
 
 }
